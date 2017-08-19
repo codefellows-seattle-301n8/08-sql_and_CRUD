491807ebf8abfe639ebdf2d36b445b063f38be3d
Joel Epstein <joel.epstein2@gmail.com>
2017-08-19 11:26:18 -0700

    comment for loadDB

--- starter-code/server.js
+++ starter-code/server.js
@@ -172,7 +172,7 @@ function loadArticles() {
 
 function loadDB() {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Put your response here...
+  // (3), C of CRUD, dont think it uses a method...
   client.query(`
     CREATE TABLE IF NOT EXISTS articles (
       article_id SERIAL PRIMARY KEY,

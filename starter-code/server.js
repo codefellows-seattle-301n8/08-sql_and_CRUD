281d3d122cc5aa24b32d4a5d20466d6e9f7e195c
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-08-19 10:05:20 -0700

    first TODO done

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,6 +4,8 @@
 const fs = require('fs');
 const express = require('express');
 
+const pg = require('pg');
+
 // REVIEW: Require in body-parser for post requests in our server. If you want to know more about what this does, read the docs!
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;

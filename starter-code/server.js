9c1cd9989750575ae43bfc961878f5ab2f130872
Joel Epstein <joel.epstein2@gmail.com>
2017-08-19 09:41:49 -0700

    completed connection string I think and installed postgres

--- starter-code/server.js
+++ starter-code/server.js
@@ -12,8 +12,7 @@ const app = express();
 // TODO: Complete the connection string for the url that will connect to your local postgres database
 // Windows and Linux users; You should have retained the user/pw from the pre-work for this course.
 // Your url may require that it's composed of additional information including user and password
-// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://joelepstein@localhost:5432/301-blog';
 
 // TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 //       This is how it knows the URL and, for Windows and Linux users, our username and password for our

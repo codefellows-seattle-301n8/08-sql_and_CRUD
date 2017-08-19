c069181b23b5a01d002a1948dce7b0f3957a48cc
Joel Epstein <joel.epstein2@gmail.com>
2017-08-19 11:12:04 -0700

    delete from articles

--- starter-code/server.js
+++ starter-code/server.js
@@ -106,7 +106,7 @@ app.put('/articles/:id', function(request, response) {
 
 app.delete('/articles/:id', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // (3), (4), DESTROY of CRUD, deleteRecord is the method...
+  // (3), (4), (5), DESTROY of CRUD, deleteRecord is the method...
   client.query(
     `DELETE FROM articles WHERE article_id=$1;`,
     [request.params.id]
@@ -121,7 +121,7 @@ app.delete('/articles/:id', function(request, response) {
 
 app.delete('/articles', function(request, response) {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // Put your response here...
+  // (3), (4), (5), DESTROY of CRUD, truncateTable - deletes all the things
   client.query(
     'DELETE FROM articles;'
   )
